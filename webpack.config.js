cce6737314938cfb9e86ccf1b47ba3f86dc999c9
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2020-10-12 21:49:36 -0500

    comment icons apple

--- webpack.config.js
+++ webpack.config.js
@@ -28,48 +28,48 @@ module.exports = {
           destination: path.join('icons', 'ios'),
           ios: true
         },
-        {
-          src: path.resolve('src/assets/apple-touch-icon-72x72.png'),
-          sizes: [72],
-          destination: path.join('icons', 'ios'),
-          ios: true
-        },
-        {
-          src: path.resolve('src/assets/apple-touch-icon-76x76.png'),
-          sizes: [76],
-          destination: path.join('icons', 'ios'),
-          ios: true
-        },
-        {
-          src: path.resolve('src/assets/apple-touch-icon-114x114.png'),
-          sizes: [114],
-          destination: path.join('icons', 'ios'),
-          ios: true
-        },
-        {
-          src: path.resolve('src/assets/apple-touch-icon-120x120.png'),
-          sizes: [120],
-          destination: path.join('icons', 'ios'),
-          ios: true
-        },
-        {
-          src: path.resolve('src/assets/apple-touch-icon-144x144.png'),
-          sizes: [144],
-          destination: path.join('icons', 'ios'),
-          ios: true
-        },
-        {
-          src: path.resolve('src/assets/apple-touch-icon-152x152.png'),
-          sizes: [152],
-          destination: path.join('icons', 'ios'),
-          ios: true
-        },
-        {
-          src: path.resolve('src/assets/apple-touch-icon-180x180.png'),
-          sizes: [180],
-          destination: path.join('icons', 'ios'),
-          ios: true
-        },
+        // {
+        //   src: path.resolve('src/assets/apple-touch-icon-72x72.png'),
+        //   sizes: [72],
+        //   destination: path.join('icons', 'ios'),
+        //   ios: true
+        // },
+        // {
+        //   src: path.resolve('src/assets/apple-touch-icon-76x76.png'),
+        //   sizes: [76],
+        //   destination: path.join('icons', 'ios'),
+        //   ios: true
+        // },
+        // {
+        //   src: path.resolve('src/assets/apple-touch-icon-114x114.png'),
+        //   sizes: [114],
+        //   destination: path.join('icons', 'ios'),
+        //   ios: true
+        // },
+        // {
+        //   src: path.resolve('src/assets/apple-touch-icon-120x120.png'),
+        //   sizes: [120],
+        //   destination: path.join('icons', 'ios'),
+        //   ios: true
+        // },
+        // {
+        //   src: path.resolve('src/assets/apple-touch-icon-144x144.png'),
+        //   sizes: [144],
+        //   destination: path.join('icons', 'ios'),
+        //   ios: true
+        // },
+        // {
+        //   src: path.resolve('src/assets/apple-touch-icon-152x152.png'),
+        //   sizes: [152],
+        //   destination: path.join('icons', 'ios'),
+        //   ios: true
+        // },
+        // {
+        //   src: path.resolve('src/assets/apple-touch-icon-180x180.png'),
+        //   sizes: [180],
+        //   destination: path.join('icons', 'ios'),
+        //   ios: true
+        // },
         {
           src: path.resolve('src/assets/ios-icon.png'),
           size: 512,
